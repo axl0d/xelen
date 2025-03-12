1e6e07e3abd5789c0b2f93aa52a419c4bd5c2cc6
Axl Orellana <axlod95@gmail.com>
2025-03-11 22:19:04 -0500

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'features/playlist/providers.dart';
+
+void main() async {
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,55 +18,76 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  Widget build(BuildContext context, WidgetRef ref) {
+    final deezer = ref.watch(deezerProvider);
+    return deezer.map(
+      data: (_) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Xelen"),
+        ),
+        body: PlayList(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.music_note_outlined),
+              label: 'Música',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border),
+              label: 'Favoritas',
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      error: (_) => Scaffold(
+        body: Center(
+          child: Text("Error no se pudo iniciar la aplicación"),
+        ),
       ),
+      loading: (_) => ColoredBox(color: Theme.of(context).colorScheme.primary),
+    );
+  }
+}
+
+class PlayList extends ConsumerWidget {
+  const PlayList({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final playlist = ref.watch(playListProvider);
+    return playlist.map(
+      data: (response) => ListView.separated(
+        separatorBuilder: (_, __) => SizedBox(height: 8),
+        itemBuilder: (_, index) {
+          final track = response.value.tracks[index];
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(4),
+              child: ListTile(
+                leading: SizedBox.square(
+                  dimension: 40,
+                  child: Image.network(track.album?.cover ?? ''),
+                ),
+                title: Text(track.title ?? "Sin título"),
+                trailing: Icon(Icons.play_arrow),
+              ),
+            ),
+          );
+        },
+        itemCount: response.value.tracks.length,
+      ),
+      error: (_) => Text("Error al cargar la playlist"),
+      loading: (_) => Center(child: CircularProgressIndicator()),
     );
   }
 }
